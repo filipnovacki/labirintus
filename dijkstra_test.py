b7b3a96852e249bd64ff720051bf333f87f4322a
Filip Novacki <otorinolaringolozi@gmail.com>
2020-01-19 22:26:04 +0100

    Dodani testni podatci s heuristikama

--- dijkstra_test.py
+++ dijkstra_test.py
@@ -15,6 +15,28 @@ def start():
     G.add_weighted_edges_from(edges)
     return G
 
-G = start()
+def startA():
+    G = nx.Graph()
+    edges = [
+            ('a', 'b', 3),
+            ('b', 'd', 5),
+            ('a', 'c', 4),
+            ('c', 'd', 2),
+            ('c', 'e', 1),
+            ('e', 'f', 1),
+            ('c', 'b', 4),
+            ('d', 'f', 8)]
+    G.add_weighted_edges_from(edges)
 
-scripts.dijkstra.dijkstra(G, 0)
+    G.nodes['a']['h'] = 6
+    G.nodes['b']['h'] = 4
+    G.nodes['c']['h'] = 4
+    G.nodes['d']['h'] = 2
+    G.nodes['e']['h'] = 2
+    G.nodes['f']['h'] = 0
+
+    return G
+
+#G = start()
+
+#scripts.dijkstra.dijkstra(G, 0)
