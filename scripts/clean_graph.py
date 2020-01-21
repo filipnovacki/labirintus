7b7d2df2cb1caba9775c7108caac1c231627023e
Filip Novacki <otorinolaringolozi@gmail.com>
2020-01-21 21:45:59 +0100

    Dodano čišćenje grafova od vrhova stupnja 1 i vrhova stupnja 2

---
+++ scripts/clean_graph.py
@@ -0,0 +1,17 @@
+def clean_graph(G):
+    delete_one = []
+    for node in G:
+        if len(G[node]) is 1:
+            delete_one.append(node)
+    G.remove_nodes_from(delete_one)
+
+    delete_two = []
+    for node in G:
+        if len(G[node]) is 2:
+            weight_sum = sum([int(G[node][a]['weight']) for a in G[node]])
+            G.add_edge(*G[node], weight = weight_sum)
+            delete_two.append(node)
+    G.remove_nodes_from(delete_two)
+
+    return G
+
