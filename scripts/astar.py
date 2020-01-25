d90f95d4f8d0a4c9cf669718ca379d3f074d8a1e
Filip Novacki <otorinolaringolozi@gmail.com>
2020-01-25 15:20:01 +0100

    Ispravljene algoritamske greške

--- scripts/astar.py
+++ scripts/astar.py
@@ -1,8 +1,24 @@
-def a_min_dist(G, dist, spath):
+from anytree import Node
+def a_min_dist(G, dist, spath, end):
     min = float('inf')
     min_index = None
 
     for node in G.nodes:
+        if dist[node][1] >= dist[end][0] and\
+                spath[node] is False and\
+                end != node:
+            spath[node] = True
+
+        if dist[node][1] + dist[node][0] > dist[end][0] and\
+                spath[node] is False and\
+                end != node:
+            spath[node] = True
+
+        if dist[node][0] > dist[end][0] and\
+                spath[node] is False and\
+                end != node:
+            spath[node] = True
+
         if dist[node][0] + dist[node][1] < min and\
                 spath[node] is False:
             min = dist[node][0] + dist[node][1]
@@ -10,17 +26,13 @@ def a_min_dist(G, dist, spath):
     return min_index
 
 def astar(G, src, end):
-    from anytree import Node
     distances = {x:[float('inf'), G.nodes[x]['h']] for x in G.nodes}
     distances[src][0] = 0
     spath = {}.fromkeys(list(G.nodes), False)
     prev = {x:Node(x) for x in G.nodes}
     prev[src] = Node(src)
     for _ in G.nodes:
-        u = a_min_dist(G, distances, spath)
-        if distances[u][0] >= distances[end][0] or \
-                u is end:
-            return distances, prev
+        u = a_min_dist(G, distances, spath, end)
         spath[u] = True
         for v in G.nodes:
             if G.has_edge(u, v) and spath[v] is False and\
--- scripts/graf_entry.py
+++ scripts/graf_entry.py
@@ -2,16 +2,23 @@ from math import sqrt
 def input_data(vertices_names, heuristic):
     edges = []
     vertices = []
+    start = ''
+    end = ''
 
     with open("data/bridovi.txt") as f:
         edges = [x[:-1].split(',') for x in f.readlines()[1:]]
 
     with open("data/vrhovi.txt") as f:
         vertices = [x[:-1].split(',') for x in f.readlines()[1:]]
+        start = min(vertices, key = lambda x: int(x[2]))[0]
+        end_list = max(vertices, key = lambda x: int(x[2]))
+        end = end_list[0]
         if heuristic == 'sqrt':
-            vertices = [x + [sqrt(pow(int(x[2]), 2)+pow(int(x[1]), 2))] for x in vertices]
+            vertices = [x + [sqrt((int(x[2]) - int(end_list[2]))**2 +\
+                (int(x[1]) - int(end_list[1]))**2)] for x in vertices]
         elif heuristic == 'pow':
-            vertices = [x +      [pow(int(x[2]), 2)+pow(int(x[1]), 2)] for x in vertices]
+            vertices = [x + [(int(x[2]) - int(end_list[2]))**2 + (int(x[1]) -\
+                int(end_list[1]))**2] for x in vertices]
 
     if vertices_names:
         with open("data/surnames.csv") as s:
@@ -24,8 +31,8 @@ def input_data(vertices_names, heuristic):
             x[0] = dictionary[x[0]]
             x[1] = dictionary[x[1]]
 
-    start = min(vertices, key = lambda x: int(x[2]))[0]
-    end = max(vertices, key = lambda x: int(x[2]))[0]
+        start = dictionary[start]
+        end = dictionary[end]
 
     edges = [(a[0], a[1], {'weight': a[2]}) for a in edges]
 
